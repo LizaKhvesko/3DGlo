f7e874039e6a6ae787683c6f6505edc811e6dde8
LizaKhvesko <khveskoliza@tut.by>
2022-03-03 10:13:24 +0300

    add helpers and modal animation

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,20 +1,11 @@
+import { animate } from './helpers'
+
 const modal = () => {
    const buttons = document.querySelectorAll('.popup-btn');
    const modal = document.querySelector('.popup');
    const modalContent = document.querySelector('.popup-content')
-   let animation;
-   let count = -70;
+   let count = 0;
    let widthScreen = window.screen.width;
-
-   function appear () {
-        count++
-        animation = requestAnimationFrame(appear);
-        if (count > 20) {
-        cancelAnimationFrame(animation)
-      } else {
-        modalContent.style.top= count * 6 + 'px'
-      }  
-   }
 
    buttons.forEach(btn => {
        btn.addEventListener('click', () => {
@@ -22,15 +13,25 @@ const modal = () => {
               modal.style.display = 'block';
            } else {
               modal.style.display = 'block';
-              appear();
-           }
+              while(count<20) {
+                 count++
+                 animate({
+                  duration: 1000,
+                  timing(timeFraction) {
+                     return timeFraction;
+                  },
+                  draw(progress) {
+                     modalContent.style.top = (count*progress) + '%';
+                  }
+               });
+              }}
        })
    })
    
    modal.addEventListener('click', (e) => {
       if(!e.target.closest('.popup-content') || e.target.classList.contains('popup-close')) {
          modal.style.display = 'none';
-         count = -70;
+         count = 0;
       }
    })
 }
